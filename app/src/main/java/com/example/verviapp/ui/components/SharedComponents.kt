a60c80365ec69cc71e5f41c0a831e7cdbaa87288
Simon <simondavidcruzsuazo@gmail.com>
2026-03-16 17:51:40 -0500

    nueva interfaz de solicitudes basica

--- app/src/main/java/com/example/verviapp/ui/components/SharedComponents.kt
+++ app/src/main/java/com/example/verviapp/ui/components/SharedComponents.kt
@@ -91,7 +91,7 @@ private data class BottomNavItem(
 fun VerviBottomBar(navController: NavController) {
     val items = listOf(
         BottomNavItem("Inicio",      Icons.Default.Home, "home"),
-        BottomNavItem("Solicitudes", Icons.Default.ListAlt, ""),
+        BottomNavItem("Solicitudes", Icons.Default.ListAlt, "request/management"),
         BottomNavItem("Historial",   Icons.Default.History, ""),
         BottomNavItem("Perfil",      Icons.Default.Person, "profile")
     )
