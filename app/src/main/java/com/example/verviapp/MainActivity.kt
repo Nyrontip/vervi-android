582e6b37ba2689826ae3f68eca19e8ee02098ab8
Luis Eduardo Sinisterra Montaño <luiseduardosinisterramontano@gmail.com>
2026-03-23 12:28:08 -0500

    feat: implement screen to see services' details

--- app/src/main/java/com/example/verviapp/MainActivity.kt
+++ app/src/main/java/com/example/verviapp/MainActivity.kt
@@ -14,6 +14,7 @@ import com.example.verviapp.ui.screens.NewRequestScreen
 import com.example.verviapp.ui.screens.NotificationsScreen
 import com.example.verviapp.ui.screens.RateServiceScreen
 import com.example.verviapp.ui.screens.RequestDetailsScreen
+import com.example.verviapp.ui.screens.ServiceDetailsScreen
 import com.example.verviapp.ui.screens.RequestsScreen
 import com.example.verviapp.ui.screens.ServiceHistoryScreen
 
@@ -67,6 +68,9 @@ class MainActivity : ComponentActivity() {
                     }
                     composable("request/details") {
                         RequestDetailsScreen(navController)
+                    }
+                    composable("service/details") {
+                        ServiceDetailsScreen(navController)
                     }
                     composable("service/apply") {
                         ApplyForServiceScreen(navController)
