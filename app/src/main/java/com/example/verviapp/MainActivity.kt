447be6a3f36c33532b8db82fa8255d17e02e8723
Luis Eduardo Sinisterra Montaño <luiseduardosinisterramontano@gmail.com>
2026-03-26 11:58:43 -0500

    feat: implementar modal para la cancelacion de una solicitud

--- app/src/main/java/com/example/verviapp/MainActivity.kt
+++ app/src/main/java/com/example/verviapp/MainActivity.kt
@@ -13,6 +13,7 @@ import com.example.verviapp.ui.screens.EditProfileScreen
 import com.example.verviapp.ui.screens.NewRequestScreen
 import com.example.verviapp.ui.screens.NotificationsScreen
 import com.example.verviapp.ui.screens.RateServiceScreen
+import com.example.verviapp.ui.screens.RequestCancelScreen
 import com.example.verviapp.ui.screens.RequestDetailsScreen
 import com.example.verviapp.ui.screens.ServiceDetailsScreen
 import com.example.verviapp.ui.screens.RequestsScreen
@@ -73,6 +74,9 @@ class MainActivity : ComponentActivity() {
                     }
                     composable("request/details") {
                         RequestDetailsScreen(navController)
+                    }
+                    composable("request/cancel") {
+                        RequestCancelScreen(navController)
                     }
                     composable("service/details") {
                         ServiceDetailsScreen(navController)
