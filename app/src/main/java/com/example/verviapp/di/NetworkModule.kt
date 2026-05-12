f31a566853d44d72ce4c23ca118a0fab9b0560d9
Simon <simondavidcruzsuazo@gmail.com>
2026-05-11 20:35:23 -0500

    fix: aceptar cualquier certificado SSL en debug para redes con inspección SSL
    - TrustManager permisivo solo cuando BuildConfig.DEBUG es true
    - En release build la validación SSL es la estándar (segura)

--- app/src/main/java/com/example/verviapp/di/NetworkModule.kt
+++ app/src/main/java/com/example/verviapp/di/NetworkModule.kt
@@ -16,8 +16,14 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
+import javax.net.ssl.HostnameVerifier
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -57,13 +63,29 @@ object NetworkModule {
             level = HttpLoggingInterceptor.Level.BODY
         }
 
-        return OkHttpClient.Builder()
+        val builder = OkHttpClient.Builder()
             .addInterceptor(authInterceptor)
             .addInterceptor(logging)
             .connectTimeout(30, TimeUnit.SECONDS)
             .readTimeout(30, TimeUnit.SECONDS)
             .writeTimeout(30, TimeUnit.SECONDS)
-            .build()
+
+        // Debug: aceptar cualquier certificado (redes con inspección SSL tipo universidad)
+        if (BuildConfig.DEBUG) {
+            val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+                override fun checkClientTrusted(chain: Array<out X509Certificate>, authType: String) {}
+                override fun checkServerTrusted(chain: Array<out X509Certificate>, authType: String) {}
+                override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+            })
+
+            val sslContext = SSLContext.getInstance("TLS")
+            sslContext.init(null, trustAllCerts, SecureRandom())
+
+            builder.sslSocketFactory(sslContext.socketFactory, trustAllCerts[0] as X509TrustManager)
+            builder.hostnameVerifier(HostnameVerifier { _, _ -> true })
+        }
+
+        return builder.build()
     }
 
     @Provides
